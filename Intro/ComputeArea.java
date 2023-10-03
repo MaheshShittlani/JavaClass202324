f50b7b6ad944316f728c37f16e201b6cae2cdce6
Mahesh Shittlani <maheshshittlani@gmail.com>
2023-10-03 09:42:51 +0530

    Day 1&2

---
+++ Intro/ComputeArea.java
@@ -0,0 +1,15 @@
+// Area of circle -> PI * radius * radius
+public class ComputeArea {
+    public static void main(String ...args) {
+        final double PI = 3.14152;
+        
+        // Input radius
+        double radius = 5.5;
+                
+        // Compute area of circle
+        double area = PI * radius * radius;
+
+        // Display area
+        System.out.println("Area of circle: " + area);
+    }
+}
---
+++ Intro/HelloWorld.java
@@ -0,0 +1,10 @@
+class Hello {
+    public static void main(String ...args) {
+        System.out.println("Welcome in JAVA!!!");
+        System.out.println("Java is Interesting");
+    }
+}
+
+class World {
+
+}
