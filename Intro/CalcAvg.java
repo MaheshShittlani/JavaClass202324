2da520459a597a90de9cb2fc01f024a1ce244cee
Mahesh Shittlani <maheshshittlani@gmail.com>
2023-10-04 09:57:10 +0530

    Java Fundamental - Day 03

---
+++ Intro/CalcAvg.java
@@ -0,0 +1,10 @@
+public class CalcAvg {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 7;
+
+        double avg = (a + b) / 2.0;
+
+        System.out.println("Average: "+avg);
+    }
+}
---
+++ Intro/Sum.java
@@ -0,0 +1,13 @@
+class Sum {
+    public static void main(String ...args) {
+        // Get two number
+        int a = 10;
+        int b = 20;
+        
+        //Calculate Sum
+        // int sum = a + b;
+
+        //Display Result
+        System.out.println("Total = " + (a + b));
+    }
+}
