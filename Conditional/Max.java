fbb599a14f7a34a7a005b86960b6df07b346c280
Mahesh Shittlani <maheshshittlani@gmail.com>
2023-10-09 09:59:26 +0530

    Day 07 | Relation operator & Logical operator

---
+++ Conditional/Max.java
@@ -0,0 +1,15 @@
+class Max {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int max;
+
+        if(a > b) {
+            max = a;
+        } else {
+            max = b;
+        }
+
+        System.out.println("Max = "+max);
+    }
+}
