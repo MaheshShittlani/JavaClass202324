5dfc547c14425ca96600a7bfb7be5e81157f841c
Mahesh Shittlani <maheshshittlani@gmail.com>
2023-11-20 10:02:34 +0530

    Object class | part-2

--- Object Class/App1.java
+++ Object Class/App1.java
@@ -26,24 +26,46 @@ class Person {
         return age;
     }
 
+    @Override
     public String toString() {
         return name + " - " + age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Person) {
+            Person p = (Person)obj;
+            return this.age == p.age && this.name.equals(p.name);
+        }
+
+        return false;
     }
 }
 
 class App1 {
     public static void main(String[] args) {
         Person p1 = new Person();
-        Person p2 = new Person();
+        Person p2 = p1;
+        Person p3 = new Person();
 
         p1.setName("Alex"); p1.setAge(22);
-        p2.setName("Clark"); p2.setAge(33);
+        p3.setName("Alex"); p3.setAge(22);
         System.out.println(p1);
         System.out.println(p2);
+        System.out.println(p3);
+
+        System.out.println(p1 == p2); // true
+        System.out.println(p1 == p3); // false
+
+        System.out.println(p1.equals(p2));
+        System.out.println(p1.equals(p3));
+        System.out.println(p1.equals("Hello"));
 
         ArrayList list = new ArrayList<>();
         list.add(1);list.add(2);list.add(3);
 
         System.out.println(list);
+
+        System.out.println(p1.hashCode());
     }    
 }
